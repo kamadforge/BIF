472c7ba9a810c488218efc561117206dd38aaabd
Kamil Adamczewski <kamil.m.adamczewski@gmail.com>
2020-05-17 13:38:38 +0200

    adding l2x evaluation metric

--- code/featimp_test.py
+++ code/featimp_test.py
@@ -434,8 +434,8 @@ def main():
 
                 path = f"models/switches_{dataset}_switch_nn_{switch_nn}_local_{training_local}.pt"
 
-                i = 49  # choose a sample
-                mini_batch_size = 5
+                i = 0  # choose a sample
+                mini_batch_size = 20000
 
                 if switch_nn==False:
                     model = Modelnn(d,2, num_samps_for_switch, mini_batch_size, point_estimate=point_estimate)
@@ -458,8 +458,58 @@ def main():
                 torch.set_printoptions(profile="full")
                 print("outputs", outputs)
                 print("phi", phi)
+                return S, datatypes_test_samp
 
-            test_instance("alternating", True, False)
+            def create_rank(scores, k):
+                # scores (100000, 10) #k - number opf ground truth relevant features, e.g. 2 or 4
+                """
+                Compute rank of each feature based on weight.
+
+                """
+                scores = abs(scores)
+                n, d = scores.shape
+                ranks = []
+                for i, score in enumerate(scores):
+                    # Random permutation to avoid bias due to equal weights.
+                    idx = np.random.permutation(d)
+                    permutated_weights = score[idx]
+                    permutated_rank = (-permutated_weights).argsort().argsort() + 1
+                    rank = permutated_rank[np.argsort(idx)]
+
+                    ranks.append(rank.numpy())
+
+                return np.array(ranks)
+
+            def compute_median_rank(scores, k, dataset, datatype_val=None):
+                ranks = create_rank(scores, k)
+                if dataset != "alternating":
+                    median_ranks = np.median(ranks[:, :k], axis=1)
+                else:
+                    datatype_val = datatype_val[:len(scores)]
+                    median_ranks1 = np.median(ranks[datatype_val == 'orange_skin', :][:, np.array([0, 1, 2, 3])],
+                                              axis=1)
+                    median_ranks2 = np.median(
+                        ranks[datatype_val == 'nonlinear_additive', :][:, np.array([4, 5, 6, 7])], axis=1)
+                    median_ranks = np.concatenate((median_ranks1, median_ranks2), 0)
+                return median_ranks
+
+
+
+            dataset="alternating"
+            S, datatypes_test_samp = test_instance(dataset, True, False)
+            if dataset=="xor":
+                k=2
+            else: #dummy for alternating
+                k=4
+
+            median_ranks = compute_median_rank(S, k, dataset, datatypes_test_samp)
+            mean_median_ranks=np.mean(median_ranks)
+            print(mean_median_ranks)
+
+            #1.5 - xor
+            #2.67 - orange_skin
+            #2.56 - nonlinear_additive
+            #2.88/ 3.5 - alternating
 
 
 
