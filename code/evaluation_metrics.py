0252a8a86f95903978cd7377f717d06508e88d97
Kamil Adamczewski <kamil.m.adamczewski@gmail.com>
2020-05-20 17:45:01 +0200

    adding fdr to evaluation metrics

--- code/evaluation_metrics.py
+++ code/evaluation_metrics.py
@@ -57,26 +57,41 @@ def get_tpr(arr1, arr2):
         lst3 = [value for value in lst1 if value in lst2]
         return lst3
 
+    def difference(arr1, arr2):
+        diff = list(set(arr2) - set(arr1))
+        return diff
+
     all = 0
-    all_corr = 0
+    all_tp = 0
+    fp = 0
     for i in range(arr1.shape[0]):
-        corr = intersection(arr1[i], arr2[i])
-        all_corr += len(corr)
+        tp = intersection(arr1[i], arr2[i])
+        diff = difference(arr1[i], arr2[i])
+
+        fp += len(diff)
+        all_tp += len(tp)
         all += len(arr1[i])
 
-    tpr = float(all_corr) / all
-    return tpr
+    fdr = float(fp) / (fp + all_tp)
+    tpr = float(all_tp) / all
+    return tpr, fdr
 
 
-def binary_classification_metrics(scores, k, dataset, mini_batch_size, datatype_val=None):
+def binary_classification_metrics(scores, k, dataset, datatype_val=None):
     tpr, fdr = 0, 0
     ranks = create_rank(scores, k)  # ranks start with 1 and end with 10 (not 0 to 9)
 
-    relevant_features_gt_positions = relevant_features = np.tile(np.arange(k) + 1, (mini_batch_size, 1))
-
     if dataset == "xor" or dataset == "orange_skin" or dataset == "nonlinear_additive":
+
+        relevant_features_gt_positions = np.tile(np.arange(k) + 1, (mini_batch_size, 1))
+
         switch_relevant_features_positions = ranks[:, :k]  # (mini_batch_size, k)
+
+
     elif dataset == "alternating":
+
+        relevant_features_gt_positions = relevant_features = np.tile(np.arange(k) + 1, (mini_batch_size, 1))
+
         datatype_val = datatype_val[:len(scores)]
         relevant_features = np.dstack([(datatype_val == 'orange_skin')] * 5) * np.array([1, 2, 3, 4, 10])
         relevant_features[0][datatype_val == 'nonlinear_additive'] = np.array([5, 6, 7, 8, 10])
@@ -89,13 +104,17 @@ def binary_classification_metrics(scores, k, dataset, mini_batch_size, datatype_
         switch_relevant_features_positions = np.array(switch_relevant_features_positions)
 
     elif "syn" in dataset:
-        relevant_features = datatype_val
-        switch_relevant_features = []
-        for i in range(relevant_features.shape[0]):
-            switch_relevant_features.append(ranks[i][relevant_features[i]])
-        switch_relevant_features = np.array(switch_relevant_features)
+        relevant_features = datatype_val + 1
 
-    tpr = get_tpr(relevant_features_gt_positions, switch_relevant_features_positions)
+        relevant_features_gt_positions = []
+        switch_relevant_features_positions = []
+        for i in range(relevant_features.shape[0]):
+            switch_relevant_features_positions.append(ranks[i][relevant_features[i] - 1])
+            relevant_features_gt_positions.append(np.arange(len(relevant_features[i])) + 1)
+        switch_relevant_features_positions = np.array(switch_relevant_features_positions)
+        relevant_features_gt_positions = np.array(relevant_features_gt_positions)
+
+    tpr, fdr = get_tpr(relevant_features_gt_positions, switch_relevant_features_positions)
 
     return tpr, fdr
 
