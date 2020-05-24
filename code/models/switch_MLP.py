1988c7d264cfe5810f407e0db8c884d503e30f40
Kamil Adamczewski <kamil.m.adamczewski@gmail.com>
2020-05-24 17:27:13 +0200

    adding back second batch norm

--- code/models/switch_MLP.py
+++ code/models/switch_MLP.py
@@ -169,7 +169,7 @@ class Model_switchlearning(nn.Module):
         
         output = self.fc1_bn1(output)
         output = nn.functional.relu(self.phi_fc2(output))
-        #output = self.fc2_bn2(output)
+        output = self.fc2_bn2(output)
         #output = nn.functional.relu(self.phi_fc2b(output))
         #output = self.fc2_bn2b(output)
 
