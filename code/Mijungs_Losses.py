ae84ffe1887239bfbe7779fddda4efafcf2ea5bc
Mijung Park <mpark@wiese.is.localnet>
2020-05-29 14:28:28 +0200

    change constant

--- code/Mijungs_Losses.py
+++ code/Mijungs_Losses.py
@@ -2,19 +2,21 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-def loss_function(prediction, true_y, phi_cand, alpha_0, hidden_dim, annealing_rate, how_many_samps):
+def loss_function(prediction, true_y, phi_cand, alpha_0, hidden_dim, annealing_rate, how_many_samps, kl_term):
 
     BCE = F.binary_cross_entropy(prediction, true_y, reduction='mean')
 
-    # KLD term
-    alpha_0 = torch.Tensor([alpha_0])
-    hidden_dim = torch.Tensor([hidden_dim])
-    trm1 = torch.lgamma(torch.sum(phi_cand)) - torch.lgamma(hidden_dim*alpha_0)
-    trm2 = - torch.sum(torch.lgamma(phi_cand)) + hidden_dim*torch.lgamma(alpha_0)
-    trm3 = torch.sum((phi_cand-alpha_0)*(torch.digamma(phi_cand)-torch.digamma(torch.sum(phi_cand))))
-    KLD = trm1 + trm2 + trm3
+    if kl_term:
+        # KLD term
+        alpha_0 = torch.Tensor([alpha_0])
+        hidden_dim = torch.Tensor([hidden_dim])
+        trm1 = torch.lgamma(torch.sum(phi_cand)) - torch.lgamma(hidden_dim*alpha_0)
+        trm2 = - torch.sum(torch.lgamma(phi_cand)) + hidden_dim*torch.lgamma(alpha_0)
+        trm3 = torch.sum((phi_cand-alpha_0)*(torch.digamma(phi_cand)-torch.digamma(torch.sum(phi_cand))))
+        KLD = trm1 + trm2 + trm3
 
-    # annealing kl-divergence term is better
+        return BCE + annealing_rate * KLD / how_many_samps
 
-    return BCE + annealing_rate*KLD/how_many_samps
-    # return BCE
+    else:
+
+        return BCE
