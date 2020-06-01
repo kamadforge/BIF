17a9b6a95f014e31319c528c876ef4ced637851e
DP-L-I <62609947+DP-L-I@users.noreply.github.com>
2020-06-01 11:32:40 +0200

    five runs

--- code/Histogram/global_explanation.py
+++ code/Histogram/global_explanation.py
@@ -29,143 +29,155 @@ from Models import Feedforward, Feature_Importance_Model
 from Losses import loss_function
 from sklearn.metrics import roc_auc_score
 
-np.random.seed(0)
+max_seed = 5
+input_dim = 10
+posterior_mean = np.zeros((max_seed,input_dim))
 
-""" generate data """
-N_tot = 10000
-# dataset = 'XOR'
-# dataset = 'orange_skin'
-dataset = 'nonlinear_additive'
-x_tot, y_tot, datatypes = generate_data(N_tot, dataset)
-y_tot = np.argmax(y_tot, axis=1)
+for seed_idx in range(max_seed):
 
-# train and test data
-N = np.int(N_tot*0.9)
-rand_perm_nums = np.random.permutation(N_tot)
-X = x_tot[rand_perm_nums[0:N], :]
-y = y_tot[rand_perm_nums[0:N]]
-Xtst = x_tot[rand_perm_nums[N:], :]
-ytst = y_tot[rand_perm_nums[N:]]
+    np.random.seed(seed_idx)
 
-""" train a classifier """
-input_dim = x_tot.shape[1]
+    """ generate data """
+    N_tot = 10000
+    dataset = 'XOR'
+    # dataset = 'orange_skin'
+    # dataset = 'nonlinear_additive'
+    x_tot, y_tot, datatypes = generate_data(N_tot, dataset)
+    y_tot = np.argmax(y_tot, axis=1)
 
-classifier = Feedforward(input_dim, 100, 20)
-criterion = torch.nn.BCELoss()
-optimizer = optim.Adam(classifier.parameters(), lr=0.05)
+    # train and test data
+    N = np.int(N_tot*0.9)
+    rand_perm_nums = np.random.permutation(N_tot)
+    X = x_tot[rand_perm_nums[0:N], :]
+    y = y_tot[rand_perm_nums[0:N]]
+    Xtst = x_tot[rand_perm_nums[N:], :]
+    ytst = y_tot[rand_perm_nums[N:]]
 
-classifier.train()
-how_many_epochs = 10
-mini_batch_size = 100
-how_many_iter = np.int(N / mini_batch_size)
+    """ train a classifier """
+    input_dim = x_tot.shape[1]
 
-for epoch in range(how_many_epochs):  # loop over the dataset multiple times
+    classifier = Feedforward(input_dim, 100, 20)
+    criterion = torch.nn.BCELoss()
+    optimizer = optim.Adam(classifier.parameters(), lr=0.05)
 
-    running_loss = 0.0
+    classifier.train()
+    how_many_epochs = 10
+    # mini_batch_size = 100
+    # how_many_iter = np.int(N / mini_batch_size)
+    mini_batch_size = N_tot  # generally larger mini batch size is more helpful for switch learning
+    how_many_iter = np.int(N / mini_batch_size)
+    if mini_batch_size==N_tot:
+        how_many_iter = 1
 
-    for i in range(how_many_iter):
-        inputs = X[i * mini_batch_size:(i + 1) * mini_batch_size, :]
-        labels = y[i * mini_batch_size:(i + 1) * mini_batch_size]
+    for epoch in range(how_many_epochs):  # loop over the dataset multiple times
 
-        optimizer.zero_grad()
-        y_pred = classifier(torch.Tensor(inputs))
-        loss = criterion(y_pred.squeeze(), torch.FloatTensor(labels))
+        running_loss = 0.0
 
-        loss.backward()
-        optimizer.step()
-        running_loss += loss.item()
+        for i in range(how_many_iter):
+            inputs = X[i * mini_batch_size:(i + 1) * mini_batch_size, :]
+            labels = y[i * mini_batch_size:(i + 1) * mini_batch_size]
 
-    y_pred = classifier(torch.Tensor(Xtst))
-    ROC = roc_auc_score(ytst, y_pred.detach().numpy())
-    print('Epoch {}: ROC : {}'.format(epoch, ROC))
+            optimizer.zero_grad()
+            y_pred = classifier(torch.Tensor(inputs))
+            loss = criterion(y_pred.squeeze(), torch.FloatTensor(labels))
 
-print('Finished Classifier Training')
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+
+        y_pred = classifier(torch.Tensor(Xtst))
+        ROC = roc_auc_score(ytst, y_pred.detach().numpy())
+        print('Epoch {}: ROC : {}'.format(epoch, ROC))
+
+    print('Finished Classifier Training')
 
 
-###########################################
+    ###########################################
 
-""" learn feature importance """
+    """ learn feature importance """
 
-# input_dim, classifier,
-num_Dir_samps = 1
-importance = Feature_Importance_Model(input_dim, classifier, num_Dir_samps)
-optimizer = optim.Adam(importance.parameters(), lr=0.075)
+    # input_dim, classifier,
+    num_Dir_samps = 1
+    importance = Feature_Importance_Model(input_dim, classifier, num_Dir_samps)
+    optimizer = optim.Adam(importance.parameters(), lr=0.075)
 
-# We freeze the classifier
-ct = 0
-for child in importance.children():
-    ct += 1
-    if ct >= 1:
-        for param in child.parameters():
-            param.requires_grad = False
+    # We freeze the classifier
+    ct = 0
+    for child in importance.children():
+        ct += 1
+        if ct >= 1:
+            for param in child.parameters():
+                param.requires_grad = False
 
-# print(list(importance.parameters())) # make sure I only update the gradients of feature importance
+    # print(list(importance.parameters())) # make sure I only update the gradients of feature importance
 
-importance.train()
-how_many_epochs = 400
-mini_batch_size = 10000  # generally larger mini batch size is more helpful for switch learning
-how_many_iter = np.int(N / mini_batch_size)
-if mini_batch_size==N_tot:
-    how_many_iter = 1
+    importance.train()
+    how_many_epochs = 400
+    mini_batch_size = N_tot  # generally larger mini batch size is more helpful for switch learning
+    how_many_iter = np.int(N / mini_batch_size)
+    if mini_batch_size==N_tot:
+        how_many_iter = 1
 
-if dataset=='orange_skin':
-    alpha_0 = 0.01
-elif dataset=='XOR':
-    alpha_0 = 0.01
-else:
-    alpha_0 = 0.05
-annealing_rate = 1  # we don't anneal. don't want to think about this.
-kl_term = True
+    if dataset=='nonlinear_additive':
+        alpha_0 = 0.05
+    else:
+        alpha_0 = 0.01
 
-for epoch in range(how_many_epochs):  # loop over the dataset multiple times
+    annealing_rate = 1  # we don't anneal. don't want to think about this.
+    kl_term = True
 
-    running_loss = 0.0
+    for epoch in range(how_many_epochs):  # loop over the dataset multiple times
 
-    for i in range(how_many_iter):
-        inputs = X[i * mini_batch_size:(i + 1) * mini_batch_size, :]
-        labels = y[i * mini_batch_size:(i + 1) * mini_batch_size]
+        running_loss = 0.0
 
-        optimizer.zero_grad()
-        y_pred, phi_cand = importance(torch.Tensor(inputs))
-        labels = torch.squeeze(torch.Tensor(labels))
-        loss = loss_function(y_pred, labels.view(-1, 1).repeat(1, num_Dir_samps), phi_cand, alpha_0, input_dim,
-                             annealing_rate, N, kl_term)
-        loss.backward()
-        optimizer.step()
-        running_loss += loss.item()
+        for i in range(how_many_iter):
+            inputs = X[i * mini_batch_size:(i + 1) * mini_batch_size, :]
+            labels = y[i * mini_batch_size:(i + 1) * mini_batch_size]
 
-    print('Epoch {}: running_loss : {}'.format(epoch, running_loss))
-    #
-    # if np.remainder(epoch, 50)==0:
-    #     # every 50th epoch, we check the results
-    #     """ checking the results """
-    #     estimated_params = list(importance.parameters())
-    #     phi_est = F.softplus(torch.Tensor(estimated_params[0]))
-    #     concentration_param = phi_est.view(-1, 1).repeat(1, 5000)
-    #     beta_param = torch.ones(concentration_param.size())
-    #     Gamma_obj = Gamma(concentration_param, beta_param)
-    #     gamma_samps = Gamma_obj.rsample()
-    #     Sstack = gamma_samps / torch.sum(gamma_samps, 0)
-    #     avg_S = torch.mean(Sstack, 1)
-    #     posterior_mean_switch = avg_S.detach().numpy()
-    #     print('estimated posterior mean of feature importance is', posterior_mean_switch)
+            optimizer.zero_grad()
+            y_pred, phi_cand = importance(torch.Tensor(inputs))
+            labels = torch.squeeze(torch.Tensor(labels))
+            loss = loss_function(y_pred, labels.view(-1, 1).repeat(1, num_Dir_samps), phi_cand, alpha_0, input_dim,
+                                 annealing_rate, N, kl_term)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
 
-print('Finished feature importance Training')
+        print('Epoch {}: running_loss : {}'.format(epoch, running_loss))
+        #
+        # if np.remainder(epoch, 50)==0:
+        #     # every 50th epoch, we check the results
+        #     """ checking the results """
+        #     estimated_params = list(importance.parameters())
+        #     phi_est = F.softplus(torch.Tensor(estimated_params[0]))
+        #     concentration_param = phi_est.view(-1, 1).repeat(1, 5000)
+        #     beta_param = torch.ones(concentration_param.size())
+        #     Gamma_obj = Gamma(concentration_param, beta_param)
+        #     gamma_samps = Gamma_obj.rsample()
+        #     Sstack = gamma_samps / torch.sum(gamma_samps, 0)
+        #     avg_S = torch.mean(Sstack, 1)
+        #     posterior_mean_switch = avg_S.detach().numpy()
+        #     print('estimated posterior mean of feature importance is', posterior_mean_switch)
 
-""" checking the results """
-estimated_params = list(importance.parameters())
-phi_est = F.softplus(torch.Tensor(estimated_params[0]))
-concentration_param = phi_est.view(-1, 1).repeat(1, 5000)
-beta_param = torch.ones(concentration_param.size())
-Gamma_obj = Gamma(concentration_param, beta_param)
-gamma_samps = Gamma_obj.rsample()
-Sstack = gamma_samps / torch.sum(gamma_samps, 0)
-avg_S = torch.mean(Sstack, 1)
-posterior_mean_switch = avg_S.detach().numpy()
-print('estimated posterior mean of feature importance is', posterior_mean_switch)
+    print('Finished feature importance Training')
 
+    """ checking the results """
+    estimated_params = list(importance.parameters())
+    phi_est = F.softplus(torch.Tensor(estimated_params[0]))
+    concentration_param = phi_est.view(-1, 1).repeat(1, 5000)
+    beta_param = torch.ones(concentration_param.size())
+    Gamma_obj = Gamma(concentration_param, beta_param)
+    gamma_samps = Gamma_obj.rsample()
+    Sstack = gamma_samps / torch.sum(gamma_samps, 0)
+    avg_S = torch.mean(Sstack, 1)
+    posterior_mean_switch = avg_S.detach().numpy()
+    print('estimated posterior mean of feature importance is', posterior_mean_switch)
+    posterior_mean[seed_idx,:] = posterior_mean_switch
+
+
+# store results
 filename = dataset+'posterior_mean.npy'
-np.save(filename, posterior_mean_switch)
+np.save(filename, posterior_mean)
 
 """ results """
 # after 600 epochs, alpha_0=0.1, mini_batch_size = 2000
