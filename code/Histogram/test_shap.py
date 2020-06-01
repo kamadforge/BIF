2e7b539c5628ecf905180118b91ba81cf81a28dc
DP-L-I <62609947+DP-L-I@users.noreply.github.com>
2020-06-01 11:32:21 +0200

    for multiple runs

--- code/Histogram/test_shap.py
+++ code/Histogram/test_shap.py
@@ -3,35 +3,45 @@ import xgboost
 from data.make_synthetic_datasets import generate_data
 import numpy as np
 
-np.random.seed(0)
 
-""" generate data """
-N_tot = 10000
-# dataset = 'XOR'
-# dataset = 'orange_skin'
-dataset = 'nonlinear_additive'
-x_tot, y_tot, datatypes = generate_data(N_tot, dataset)
-y_tot = np.argmax(y_tot, axis=1)
+max_seed = 5
+input_dim = 10
+sv = np.zeros((max_seed,input_dim))
 
-# train and test data
-N = np.int(N_tot*0.9)
-rand_perm_nums = np.random.permutation(N_tot)
-X = x_tot[rand_perm_nums[0:N], :]
-y = y_tot[rand_perm_nums[0:N]]
+for seed_idx in range(max_seed):
 
+    np.random.seed(seed_idx)
 
-model = xgboost.train({"learning_rate": 0.01}, xgboost.DMatrix(X, label=y), 500)
-
-explainer = shap.TreeExplainer(model)
-shap_values = explainer.shap_values(X)
+    """ generate data """
+    N_tot = 10000
+    dataset = 'XOR'
+    # dataset = 'orange_skin'
+    # dataset = 'nonlinear_additive'
+    x_tot, y_tot, datatypes = generate_data(N_tot, dataset)
+    y_tot = np.argmax(y_tot, axis=1)
 
 
-mean_sv = np.abs(shap_values).mean(axis=0)
-mean_sv_arg=np.argsort(mean_sv)[::-1]
+    # train and test data
+    N = np.int(N_tot*0.9)
+    rand_perm_nums = np.random.permutation(N_tot)
+    X = x_tot[rand_perm_nums[0:N], :]
+    y = y_tot[rand_perm_nums[0:N]]
 
-print(mean_sv)
-print(mean_sv_arg)
+
+    model = xgboost.train({"learning_rate": 0.01}, xgboost.DMatrix(X, label=y), 100)
+
+    explainer = shap.TreeExplainer(model)
+    shap_values = explainer.shap_values(X)
+
+
+    mean_sv = np.abs(shap_values).mean(axis=0)
+    # mean_sv_arg=np.argsort(mean_sv)[::-1]
+    sv[seed_idx,:] = mean_sv
+
+
+# print(mean_sv)
+# print(mean_sv_arg)
 
 
 filename = dataset+'shap.npy'
-np.save(filename, mean_sv)
+np.save(filename, sv)
